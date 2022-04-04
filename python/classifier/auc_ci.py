ce70627ff9b272f4e045b7792930aa8416d935f4
Tudor Manole <tmanole@andrew.cmu.edu>
2022-04-04 19:01:17 -0400

    bootstrap test for AUC CI

--- python/classifier/auc_ci.py
+++ python/classifier/auc_ci.py
@@ -1,5 +1,113 @@
 import numpy as np
 import scipy.stats
+from roc_auc_with_negative_weights import roc_auc_with_negative_weights
+import matplotlib.pyplot as plt
+
+
+def bootstrap(x, y, weights, alpha=0.05, B=1000, o=0):
+    """ Compute a bootstrap confidence interval for the weighted AUC.
+
+        Args:
+            x(Numpy array): Classification probabilities for the class 0 (typically "3b" or "CR").
+            y(Numpy array): Classification probabilities for the class 1 (typically "4b" or "SR").
+            w(Numpy array): Weights.
+            auc(float): Estimate of the area under ROC curve.
+            alpha(float): Confidence interval level.
+
+        Returns:
+            List: Lower and upper confidence bounds for the AUC.
+    """
+    n = x.shape[0]
+
+    auc = roc_auc_with_negative_weights(x, y, weights=weights)
+    print(auc) 
+    aucs = []
+
+    tinds = np.empty((B,n))
+
+    for b in range(B):
+        print("boot ",  b)
+        inds = np.random.choice(a=n, size=n, replace=True).flatten().tolist()#, p=weights/np.sum(weights)).flatten().tolist()
+#        print(inds) 
+#        print(x.shape)
+#        print(inds)
+
+
+        """
+        for i in range(n):
+            if weights[inds[i]] == 0:
+                print("offensive ind: ", i)
+        """
+        print("Unique inds: ", np.unique(inds).shape)
+        tinds[b,inds] += 1
+
+        xx = x[inds].flatten()
+        yy = y[inds].flatten()
+        ww = weights[inds].flatten()
+#        print(xx.shape)
+#        print(np.sum(x[weights > 0])) 
+#        print(np.sum(xx[ww > 0])) 
+
+        aucs.append(roc_auc_with_negative_weights(xx, yy, weights=ww))
+            
+#    print("uniques")
+#    print(np.unique(tinds, axis=0))
+#    print(np.unique(tinds, axis=1))
+#
+#    print(np.sum(tinds[b,:]==0))
+#    print(np.sum(np.unique(tinds, axis=1)==0))
+#
+#    print("@")
+#    print(np.where(~tinds.transpose().any(axis=1))[0].shape)
+#    print(np.sum(weights==0))
+
+#    print("offensive inds")
+#    for i in range(n):g
+#        if tinds[0,i] > 0 and weights[i] == 0:
+#            print(i)
+#
+
+    plt.hist(aucs)
+    plt.savefig("boot" + str(o) + ".pdf") 
+
+    se = np.sqrt(np.var(aucs))
+    print("Standard error: ", se)
+
+    q = scipy.stats.norm.ppf(1-alpha/2)
+
+    return [auc - q * se, auc + q * se]
+
+
+x1 = np.load("../classes1.npy")
+y1 = np.load("../preds1.npy")
+w1 = np.load("../weights1.npy")
+
+x2 = np.load("../classes10.npy")
+y2 = np.load("../preds10.npy")
+w2 = np.load("../weights10.npy")
+
+x3 = np.load("../classes100.npy")
+y3 = np.load("../preds100.npy")
+w3 = np.load("../weights100.npy")
+
+print(np.sum(w1<1e-9))
+print(np.sum(w2<1e-9))
+print(np.sum(w3<1e-9))
+print(w1.shape)
+print(w2.shape)
+print(w3.shape)
+ci1 = bootstrap(x1, y1, w1, o=1)
+ci2 = bootstrap(x2, y2, w2, o=2)
+ci3 = bootstrap(x3, y3, w3, o=3)
+
+print(ci1)
+print(ci2)
+print(ci3)
+
+print(ci1[1] - ci1[0])
+print(ci2[1] - ci2[0])
+print(ci3[1] - ci3[0])
+
 
 def bamber(x, y, auc, alpha=0.05):
     """ Compute the Bamber(1975) Wald CI for the AUC.
